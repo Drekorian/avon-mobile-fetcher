86bc3fc32d97c47b69d7dfee8e8341b03ecd844e
Marek Osvald <drekorian@drekorian.cz>
2022-07-02 15:36:03 +0200

    Update dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     java
-    kotlin("jvm") version "1.6.10"
-    kotlin("plugin.serialization") version "1.6.10"
+    kotlin("jvm") version "1.7.0"
+    kotlin("plugin.serialization") version "1.7.0"
 }
 
 group = "cz.drekorian.avonmobilefetcher"
@@ -14,19 +14,20 @@ repositories {
     mavenCentral()
 }
 
+@Suppress("SpellCheckingInspection")
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.6.10")
-    implementation("io.ktor:ktor-client-cio:1.6.8")
-    implementation("io.ktor:ktor-client-core:1.6.8")
-    implementation("io.ktor:ktor-client-logging:1.6.8")
-    implementation("io.ktor:ktor-client-serialization:1.6.8")
-    implementation("io.github.microutils:kotlin-logging:2.1.21")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.7.0")
+    implementation("io.ktor:ktor-client-cio:2.0.2")
+    implementation("io.ktor:ktor-client-core:2.0.2")
+    implementation("io.ktor:ktor-client-logging:2.0.2")
+    implementation("io.ktor:ktor-client-serialization:2.0.2")
+    implementation("io.github.microutils:kotlin-logging:2.1.23")
     implementation("org.slf4j", "slf4j-simple", "1.7.25")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.8.2")
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.8.2")
-    testImplementation("org.assertj:assertj-core:3.22.0")
-    testImplementation("com.natpryce:hamkrest:1.8.0.1")
+    testImplementation("org.assertj:assertj-core:3.23.1")
+    testImplementation("com.timeular.nytta.prova:hamkrest:5.3.1")
 }
 
 configure<JavaPluginExtension> {
